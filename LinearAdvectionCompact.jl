5add59b3b540bf51cd92f824de99ed3630d744e8
Kate88L <katarina@lacko.eu>
2023-06-21 10:24:15 +0200

    fixed

--- LinearAdvectionCompact.jl
+++ LinearAdvectionCompact.jl
@@ -8,11 +8,10 @@ include("InitialFunctions.jl")
 ## Definition of basic parameters
 
 # Level of refinement
-level = 1
+level = 2;
 
 # Courant number
 c = 1.5
-c_inv = 1 / c
 
 # Grid settings
 xL = - pi / 2
@@ -28,7 +27,7 @@ tau = c * h / u
 Ntau = Int(Nx / 10)
 
 # Initial condition
-phi_0(x) = piecewiseLinear(x);
+phi_0(x) = cos(x);
 
 # Exact solution
 phi_exact(x, t) = phi_0(x - u * t);
@@ -38,18 +37,21 @@ phi_exact(x, t) = phi_0(x - u * t);
 # Grid initialization
 x = range(xL, xR, length = Nx + 1)
 phi = zeros(Nx + 1, Ntau + 1);
-phi_predictor = zeros(Nx + 1, Ntau + 1);
+phi_predictor = zeros(Nx + 1, Ntau + 1); # predictor in time n+1
+phi_predictor_n2 = zeros(Nx + 1, Ntau + 1); # predictor in time n+2
 phi_first_order = zeros(Nx + 1, Ntau + 1);
 
 # Initial condition
 phi[:, 1] = phi_0.(x);
 phi_predictor[:, 1] = phi_0.(x);
+phi_predictor_n2[:, 1] = phi_0.(x);
 phi_first_order[:, 1] = phi_0.(x);
 
 # Boundary conditions
 phi[1, :] = phi_exact.(x[1], range(0, Ntau * tau, length = Ntau + 1));
 phi_predictor[1, :] = phi_exact.(x[1], range(0, Ntau * tau, length = Ntau + 1));
 phi_first_order[1, :] = phi_exact.(x[1], range(0, Ntau * tau, length = Ntau + 1));
+phi_predictor_n2[1, :] = phi_exact.(x[1], range(tau, (Ntau + 1) * tau, length = Ntau + 1));
 
 # Ghost point on the time -1
 ghost_point_time = phi_exact.(x, -tau);
@@ -73,18 +75,17 @@ for n = 1:Ntau
         phi_first_order[i, n + 1] = ( phi_first_order[i, n] + c * phi_first_order[i - 1, n + 1] ) / ( 1 + c );
         
         # Predictor
-        phi_predictor[i, n + 1] = ( phi_predictor[i, n] + c * phi[i - 1, n + 1] ) / ( 1 + c );
+        phi_predictor[i, n + 1] = ( phi[i, n] + c * phi_predictor[i - 1, n + 1] ) / ( 1 + c );
 
         # Corrector
-        r_downwind = - phi_predictor[i, n + 1] + phi_predictor[i - 1, n + 1] + phi[i, n] - phi[i - 1, n + 1];
+        r_downwind = - phi_predictor[i, n + 1] + phi_predictor_n2[i - 1, n + 1] + phi[i, n] - phi[i - 1, n + 1];
         r_upwind = phi[i - 1, n + 1] - phi[i - 1, n] - phi[i, n] + phi_old[i];
 
         # Second order solution
-        phi[i, n + 1] = ( phi[i - 1, n + 1] + c_inv * ( phi[i, n] - 0.5 * ( 1 - w ) * r_downwind - 0.5 * w * r_upwind )) / ( 1 + c_inv );
+        phi[i, n + 1] = ( phi[i, n] + c * phi[i - 1, n + 1] - 0.5 * (1 - w) * r_downwind - 0.5 * w * r_upwind ) / ( 1 + c );
 
         # Predictor for next time step
-        # phi_predictor[i, n + 1]  = ( phi[i - 1, n + 1] + c_inv * phi_predictor[i, n] )  / ( 1 + c_inv );
-        phi_predictor[i, n + 1] = ( phi_predictor[i, n] + c * phi[i - 1, n + 1] ) / ( 1 + c );
+        phi_predictor_n2[i, n + 1] = ( phi[i, n + 1] + c * phi_predictor_n2[i - 1, n + 1] ) / ( 1 + c );
 
     end
 end

0d786fb2e29f5862f3673cf0443b31b95c1e3b2b
Kate88L <katarina@lacko.eu>
2023-06-23 12:51:00 +0200

    fixed ghost points on the left side

--- LinearAdvectionCompactNormal.jl
+++ LinearAdvectionCompactNormal.jl
@@ -8,7 +8,7 @@ include("InitialFunctions.jl")
 ## Definition of basic parameters
 
 # Level of refinement
-level = 1;
+level = 0;
 
 # Courant number
 c = 0.5
@@ -47,21 +47,19 @@ phi_first_order[:, 1] = phi_0.(x);
 
 # Boundary conditions
 phi[1, :] = phi_exact.(x[1], range(0, Ntau * tau, length = Ntau + 1));
-phi[2, :] = phi_exact.(x[2], range(0, Ntau * tau, length = Ntau + 1));
-phi[Nx + 1, :] = phi_exact.(x[Nx + 1], range(0, Ntau * tau, length = Ntau + 1));
-
 phi_first_order[1, :] = phi_exact.(x[1], range(0, Ntau * tau, length = Ntau + 1));
-phi_first_order[2, :] = phi_exact.(x[2], range(0, Ntau * tau, length = Ntau + 1));
-phi_first_order[Nx + 1, :] = phi_exact.(x[Nx + 1], range(0, Ntau * tau, length = Ntau + 1));
+phi_predictor[1, :] = phi_exact.(x[1], range(0, Ntau * tau, length = Ntau + 1));
 
 # Ghost point on the right side (u > 0)
 ghost_point_right = phi_exact.(xR + h, range(0, Ntau * tau, length = Ntau + 1));
+# Ghost point on the left side 
+ghost_point_left = phi_exact.(xL - h, range(tau, (Ntau+1) * tau, length = Ntau + 1));
 
 # ENO parameters
 w = 1 / 2;
 
 # Space loop
-for i = 3:Nx+1
+for i = 2:Nx+1
     # Time loop
     for n = 1:Ntau
 
@@ -69,6 +67,12 @@ for i = 3:Nx+1
             phi_right = phi_predictor[i + 1, n];
         else
             phi_right = ghost_point_right[n];
+        end
+
+        if i > 2
+            phi_left_n_plus = phi[i - 2, n + 1];
+        else
+            phi_left_n_plus = ghost_point_left[n];
         end
 
         # First order solution
@@ -79,7 +83,7 @@ for i = 3:Nx+1
 
         # Corrector
         r_downwind = - phi_predictor[i, n + 1] + phi_right - phi[i, n] + phi[i - 1, n + 1];
-        r_upwind = phi[i, n] - phi[i - 1, n] - phi[i - 1, n + 1] + phi[i - 2, n + 1];
+        r_upwind = phi[i, n] - phi[i - 1, n] - phi[i - 1, n + 1] + phi_left_n_plus;
 
         # ENO parameter
         # if abs(r_downwind) >= abs(r_upwind)

4d268a1e7a566ab1d1e7b15a6ca44b74e54c444f
Kate88L <katarina@lacko.eu>
2023-12-07 13:01:28 +0100

    2d dimension splitting - only first order for some reason

--- DimensionalSplitting_normal.jl
+++ DimensionalSplitting_normal.jl
@@ -27,7 +27,7 @@ U = [1.0, 1.0]
 # Time settings
 tau = C * h / maximum(abs.(U))
 Ntau = Int(Nx / 10)
-Ntau = 1
+# Ntau = 1
 
 c = zeros(Nx + 1, 1) .+ U[1] * tau / h;
 d = zeros(Nx + 1, 1) .+ U[2] * tau / h;
@@ -73,27 +73,24 @@ for n = 1:Ntau
 
     # Boundary conditions
     phi_first_order[1, :, n + 1] = phi_exact_x.(x1[1], x2, n * tau, (n-1)* tau);
-
     phi[1, :, n + 1] = phi_exact_x.(x1[1], x2, n * tau, (n-1)* tau);
-
     phi_predictor[1, :, n + 1] = phi_exact_x.(x1[1], x2, n * tau, (n-1)* tau);
-
-    ghost_point_left = phi_exact_x.(x1L-h, X2, n * tau, (n-1)* tau);
-    ghost_point_right = phi_exact_x.(x1R+h, X2, (n-1) * tau, (n-2)* tau);
+    ghost_point_left = phi_exact_x.(x1[1]-h, x2, n * tau, (n-1)* tau);
+    ghost_point_right = phi_exact_x.(x1[end]+h, x2, (n-1) * tau, (n-2)* tau);
 
     # Space loop - X direction
     for  i = 2:1:Nx + 1
  
-        if i < Nx + 1
-            phi_i_plus = phi_predictor[i + 1, :, n];
-        else
-            phi_i_plus = ghost_point_right[n];
-        end
-
         if i > 2
             phi_left = phi[i - 2, :, n + 1];
         else
-            phi_left = ghost_point_left[n];
+            phi_left = ghost_point_left;
+        end
+
+        if i < Nx + 1
+            phi_right = phi_predictor[i + 1, :, n];
+        else
+            phi_right = ghost_point_right;
         end
 
         # First order solution
@@ -104,78 +101,70 @@ for n = 1:Ntau
 
         # Corrector
         r_downwind_i_minus = - phi_predictor[i, :, n] + phi_predictor[i - 1, :, n + 1];
-        r_upwind_i_minus = -phi[i - 1, :, n] .+ phi_left;
+        r_upwind_i_minus = -phi[i - 1, :, n] + phi_left;
 
-        r_downwind_i = - phi_predictor[i, :, n + 1] .+ phi_i_plus;
+        r_downwind_i = - phi_predictor[i, :, n + 1] + phi_right;
         r_upwind_i = phi[i, :, n] - phi[i - 1, :, n + 1];
 
         r_upwind_i += r_upwind_i_minus;
         r_downwind_i += r_downwind_i_minus;
 
         # ENO parameter
-        abs.(r_downwind_i) <= abs.(r_upwind_i) ? sx[i, n + 1] = 1 : sx[i, n + 1] = 1;
+        abs.(r_downwind_i) <= abs.(r_upwind_i) ? sx[i, n + 1] = 1 : sx[i, n + 1] = 0;
 
         # Second order solution
         phi[i, :, n + 1] = ( phi[i, :, n] + c[i] * phi[i - 1, :, n + 1] - 0.5 * sx[i, n + 1] * r_downwind_i - 0.5 * ( 1 - sx[i, n + 1] ) * r_upwind_i )  / ( 1 + c[i] );
 
     end
 
-    # phi[:, :, n] = phi[:, :, n + 1];
-    # phi_first_order[:, :, n] = phi_first_order[:, :, n + 1];
+    phi[:, :, n] = phi[:, :, n + 1];
+    phi_first_order[:, :, n] = phi_first_order[:, :, n + 1];
 
-    # phi_first_order[1, :, n + 1] = phi_exact.(x1[1], x2, n * tau);
-    # phi_first_order[:, 1, n + 1] = phi_exact.(x1, x2[1], n * tau);
+    phi_first_order[:, 1, n + 1] = phi_exact.(x1, x2[1], n * tau);
+    phi[:, 1, n + 1] = phi_exact.(x1, x2[1], n * tau);
+    phi_predictor[:, 1, n + 1] = phi_exact.(x1, x2[1], n * tau);
 
-    # phi[1, :, n + 1] = phi_exact.(x1[1], x2, n * tau);
-    # phi[:, 1, n + 1] = phi_exact.(x1, x2[1], n * tau);
+    ghost_point_down = phi_exact.(x1, x2[1]-h, n * tau);
+    ghost_point_up = phi_exact.(x1, x2[end]+h, (n-1) * tau);
 
-    # phi_predictor[1, :, n + 1] = phi_exact.(x1[1], x2, n * tau);
-    # phi_predictor[:, 1, n + 1] = phi_exact.(x1, x2[1], n * tau);
+    # Space loop - Y direction
+    for j = 2:1:Nx + 1
 
-    # ghost_point_left = phi_exact.(x1L-h, x2, n * tau);
-    # ghost_point_right = phi_exact.(x1R+h, x2, (n-1) * tau);
-    # ghost_point_up = phi_exact.(x1, x2R+h, (n-1) * tau);
-    # ghost_point_down = phi_exact.(x1, x2L-h, n * tau);
+        if j > 2
+            phi_down = phi[:, j - 2, n + 1];
+        else
+            phi_down = ghost_point_down;
+        end
 
-    # # Space loop - Y direction
-    # for j = 2:1:Nx + 1
-    #     i = 2:1:Nx + 1;
+        if j < Nx + 1
+            phi_up = phi_predictor[:, j + 1, n];
+        else
+            phi_up = ghost_point_up;
+        end
 
-    #     if j < Nx 
-    #         phi_up = phi[:, j + 1, n];
-    #     else
-    #         phi_up = ghost_point_up[n];
-    #     end
-
-    #     if j > 2
-    #         phi_down = phi[:, j - 2, n + 1];
-    #     else
-    #         phi_down = ghost_point_down[n];
-    #     end
-
-    #     # First order solution
-    #     phi_first_order[i, j, n + 1] = ( phi_first_order[i, j, n] + d[j] * phi_first_order[i, j - 1, n + 1] ) / ( 1 + d[j] );
+        # First order solution
+        phi_first_order[:, j, n + 1] = ( phi_first_order[:, j, n] + d[j] * phi_first_order[:, j - 1, n + 1] ) / ( 1 + d[j] );
         
-    #     # Predictor
-    #     phi_predictor[:, j, n + 1] = ( phi_predictor[:, j, n] + d[j] * phi[:, j - 1, n + 1] ) / ( 1 + d[j] );
+        # Predictor
+        phi_predictor[:, j, n + 1] = ( phi_predictor[:, j, n] + d[j] * phi[:, j - 1, n + 1] ) / ( 1 + d[j] );
 
-    #     # Corrector
-    #     r_downwind_j_minus = - phi_predictor[:, j, n] + phi_predictor[:, j - 1, n + 1];
-    #     r_upwind_j_minus = - phi[:, j - 1, n] .+ phi_down;
+        # Corrector
+        r_downwind_j_minus = - phi_predictor[:, j, n] + phi_predictor[:, j - 1, n + 1];
+        r_upwind_j_minus = - phi[:, j - 1, n] + phi_down;
 
-    #     r_downwind_j = - phi_predictor[:, j, n + 1] + phi_predictor[:, j - 1, n];
-    #     r_upwind_j = phi[:, j, n] - phi[:, j - 1, n + 1];
+        r_downwind_j = - phi_predictor[:, j, n + 1] + phi_up;
+        r_upwind_j = phi[:, j, n] - phi[:, j - 1, n + 1];
 
-    #     r_upwind_j += r_upwind_j_minus;
-    #     r_downwind_j += r_downwind_j_minus; 
+        r_upwind_j += r_upwind_j_minus;
+        r_downwind_j += r_downwind_j_minus; 
 
-    #     # ENO parameter 
-    #     abs.(r_downwind_j) <= abs.(r_upwind_j) ? sy[j, n + 1] = 1 : sy[j, n + 1] = 0;
+        # ENO parameter 
+        abs.(r_downwind_j) <= abs.(r_upwind_j) ? sy[j, n + 1] = 1 : sy[j, n + 1] = 0;
 
-    #     # Second order solution
-    #     phi[:, j, n + 1] = ( phi[:, j, n] + d[j] * phi[:, j - 1, n + 1] - 0.5 * sy[j, n + 1] * r_downwind_j - 0.5 * ( 1 - sy[j, n + 1] ) * r_upwind_j )  / ( 1 + d[j] );
+        # Second order solution
+        phi[:, j, n + 1] = ( phi[:, j, n] + d[j] * phi[:, j - 1, n + 1] - 0.5 * sy[j, n + 1] * r_downwind_j - 0.5 * ( 1 - sy[j, n + 1] ) * r_upwind_j )  / ( 1 + d[j] );
 
-    # end
+    end
 end
 
 # Print the error
@@ -187,12 +176,12 @@ println("Error L2 first order: ", sum(abs.(phi_first_order[:, :, end] - phi_exac
 println("Error L_inf firts order: ", norm(phi_first_order[:, :, end] - phi_exact.(X1, X2, Ntau * tau), Inf)* h^2)
 
 # Plot of the result at the final time together with the exact solution
-trace1 = contour(x = x1, y = x2, z = phi_exact_x.(X1, X2, tau, 0), name = "Exact solution", showscale=false, contours_coloring="lines", colorscale="Greys", line_width=2)
+trace1 = contour(x = x1, y = x2, z = phi_exact.(X1, X2, Ntau * tau), name = "Exact solution", showscale=false, contours_coloring="lines", colorscale="Greys", line_width=2)
 trace2 = contour(x = x1, y = x2, z = phi[:, :, end], name = "Compact", showscale=false, colorscale = "Plasma", contours_coloring="lines", line_width=1)
 trace3 = contour(x = x1, y = x2, z = phi_first_order[:, :, end], name = "First order", showscale=false, colorscale = "Viridis", contours_coloring="lines", line_width=1)
 layout = Layout(title = "Linear advection equation", xaxis_title = "x2", yaxis_title = "x1", zaxis_title = "phi", colorbar = false)
 
 plot_phi = plot([trace1, trace2], layout)
 
-# plot_error = plot(surface(x = x1, y = x2, z = abs.(phi[:, :, end] - phi_exact.(X1, X2, Ntau * tau))* h^2))
+plot_error = plot(surface(x = x1, y = x2, z = abs.(phi[:, :, end] - phi_exact.(X1, X2, Ntau * tau))* h^2))
 

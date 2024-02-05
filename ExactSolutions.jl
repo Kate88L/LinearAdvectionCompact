5cd683b471ab62b2200b093bf83102ba605e264c
Kate88L <katarina@lacko.eu>
2024-02-05 13:47:57 +0100

    new function to test

---
+++ ExactSolutions.jl
@@ -0,0 +1,17 @@
+## Exact solutionS
+
+# Exact solution for the advection equation for Velocity = 2 + 3/2 * cos(x) and initial condition = cos(x)
+function cosVelocitySmooth(x, t)
+    return cos.(2*atan.(sqrt(7).*tan((sqrt(7).*(t - (4*atan.(tan.(x/2)./sqrt(7)))./sqrt(7)))/4)))
+end
+
+# Exact solution for the advection equation for Velocity = sin(x) and initial condition = sin(x)
+function sinVelocitySmooth(x, t)
+    return sin.(2 * atan.( tan.(x / 2) * exp.(-t) ))
+end
+
+# Exact solution for the advection equation for Velocity = 1 + 3/4 * cos(x) and initial condition = arcsin( sin(x + Pi/2) * 2 / Pi )
+function cosVelocityNonSmooth(x, t)
+    return (2 * asin(cos(2 * atan(sqrt(7) * tan((1/8) * sqrt(7) * (t - (8 * atan(tan(x/2) / sqrt(7))) / sqrt(7))))))) / π
+
+end
--- LinearAdvectionCompactNormal.jl
+++ LinearAdvectionCompactNormal.jl
@@ -6,46 +6,45 @@ using CSV
 using DataFrames
 
 include("InitialFunctions.jl")
+include("ExactSolutions.jl")
 
 ## Definition of basic parameters
 
 # Level of refinement
-level = 3;
+level = 0;
 
 # Courant number
-C = 5
+C = 1
 
 # Grid settings
-xL = - 1
-xR = 1
+xL = - 1 * π / 2
+xR = 1 * 3 * π / 2
 Nx = 100 * 2^level
 h = (xR - xL) / Nx
 
 # Velocity
-u = 1.0
-
-# Time settings
-T = 1
-Ntau = 10 * 2^level
-tau = T / Ntau
-
-c = u * tau / h
+u(x) = 1 + 3/4 * cos(x)
+# u(x) = 1
 
 # Initial condition
-# phi_0(x) = piecewiseLinear(x);
-# phi_0(x) = piecewiseConstant(x);
-phi_0(x) = makePeriodic(nonSmooth,-1,1)(x - 0.5);
-# phi_0(x) = cos(x);
-# phi_0(x) = makePeriodic(allInOne,-1,1)(x);
-# phi_0(x) = makePeriodic(continuesMix,-1,1)(x);
+phi_0(x) = asin( sin(x + π/2) ) * 2 / π;
+# phi_0(x) = cos.(x);
 
 # Exact solution
-phi_exact(x, t) = phi_0(x - u * t);
-
-## Comptutation
+phi_exact(x, t) = cosVelocityNonSmooth(x, t);
+# phi_exact(x, t) = phi_0.(x - t);
 
 # Grid initialization
 x = range(xL, xR, length = Nx + 1)
+
+# Time settings
+T = 2 * π
+Ntau = 10 * 2^level
+tau = T / Ntau
+
+c = zeros(Nx+1,1) .+ u.(x) * tau / h
+
+## Comptutation
 phi = zeros(Nx + 1, Ntau + 1);
 phi_predictor = zeros(Nx + 1, Ntau + 1);
 phi_first_order = zeros(Nx + 1, Ntau + 1);
@@ -64,8 +63,6 @@ phi[end, :] = phi_exact.(x[end], range(0, Ntau * tau, length = Ntau + 1));
 phi_first_order[end, :] = phi_exact.(x[end], range(0, Ntau * tau, length = Ntau + 1));
 phi_predictor[end, :] = phi_exact.(x[end], range(0, Ntau * tau, length = Ntau + 1));
 
-# Ghost point on the right side (u > 0)
-ghost_point_right = phi_exact.(xR + h, range(0, Ntau * tau, length = Ntau + 1));
 # Ghost point on the left side 
 ghost_point_left = phi_exact.(xL - h, range(tau, (Ntau+1) * tau, length = Ntau + 1));
 
@@ -76,13 +73,7 @@ eps = 1e-8;
 # Time loop
 for n = 1:Ntau
     # Space loop
-    for  i = 2:1:Nx
-
-        if i < Nx 
-            phi_right = phi[i + 2, n + 1];
-        else
-            phi_right = ghost_point_right[n];
-        end
+    for  i = 2:1:Nx + 1
 
         if i > 2
             phi_left = phi[i - 2, n + 1];
@@ -90,20 +81,24 @@ for n = 1:Ntau
             phi_left = ghost_point_left[n];
         end
 
+        if i < Nx + 1
+            phi_right = phi_predictor[i + 1, n];
+        else
+            phi_right = phi_exact(xR + h, (n-1) * tau) ;
+        end
+
         # First order solution
-        phi_first_order[i, n + 1] = ( phi_first_order[i, n] + abs(c) * (c > 0) * phi_first_order[i - 1, n + 1] 
-                                                            + abs(c) * (c < 0) * phi_first_order[i + 1, n + 1] ) / ( 1 + abs(c) );
+        phi_first_order[i, n + 1] = ( phi_first_order[i, n] + c[i] *  phi_first_order[i - 1, n + 1] ) / ( 1 + c[i] );
         
         # Predictor
-        phi_predictor[i, n + 1] = ( phi_predictor[i, n] + abs(c) * (c > 0) * phi[i - 1, n + 1] 
-                                                        + abs(c) * (c < 0) * phi[i + 1, n + 1] ) / ( 1 + abs(c) );
+        phi_predictor[i, n + 1] = ( phi_predictor[i, n] + c[i] *  phi[i - 1, n + 1] ) / ( 1 + c[i] );
 
         # Corrector
-        r_downwind_i_minus = - phi_predictor[i, n] + (c > 0) * phi_predictor[i - 1, n + 1] + (c < 0) * phi_predictor[i + 1, n + 1];
-        r_upwind_i_minus = - (c > 0) * phi[i - 1, n] - (c < 0) * phi[i + 1, n] + (c > 0) * phi_left + (c < 0) * phi_right;
+        r_downwind_i_minus = - phi_predictor[i, n] + phi_predictor[i - 1, n + 1];
+        r_upwind_i_minus = - phi[i - 1, n] + phi_left;
 
-        r_downwind_i = - phi_predictor[i, n + 1] + (c > 0) * phi_predictor[i + 1, n] + (c < 0) * phi_predictor[i - 1, n];
-        r_upwind_i = phi[i, n] - (c > 0) * phi[i - 1, n + 1] - (c < 0) * phi[i + 1, n + 1];
+        r_downwind_i = - phi_predictor[i, n + 1] + phi_right;
+        r_upwind_i = phi[i, n] - phi[i - 1, n + 1];
 
         # ENO parameter
         if abs(r_downwind_i_minus + r_downwind_i) <= abs(r_upwind_i_minus + r_upwind_i)
@@ -113,7 +108,7 @@ for n = 1:Ntau
         end
 
         # Second order solution
-        phi[i, n + 1] = ( phi[i, n] + abs(c) * ( (c > 0) * phi[i - 1, n + 1] + (c < 0) * phi[i + 1, n + 1] - 0.5 * s[i, n + 1] * (r_downwind_i_minus + r_downwind_i) - 0.5 * (1-s[i, n + 1]) * (r_upwind_i + r_upwind_i_minus ) ) ) / ( 1 + abs(c) );
+        phi[i, n + 1] = ( phi[i, n] + c[i] * ( phi[i - 1, n + 1] - 0.5 * s[i, n + 1] * (r_downwind_i_minus + r_downwind_i) - 0.5 * (1-s[i, n + 1]) * (r_upwind_i + r_upwind_i_minus ) ) ) / ( 1 + c[i] );
     end
 
 end
